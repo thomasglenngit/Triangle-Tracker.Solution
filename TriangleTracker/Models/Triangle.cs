b94e596c15038902a6ebff936de1e7e3992977e5
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-14 17:02:54 -0600

    add program.cs file and exe file to .csproj.
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -1,11 +1,32 @@
-namespace TriangleTracker
+using System;
+
+namespace TriangleTracker.Models
 {
-  public class Triangle 
+  public class Triangle
   {
+    public static void Main()
+    {
+      Console.WriteLine("Enter a value for side one.");
+      string strSide1 = Console.ReadLine();
+      int side1 = int.Parse(strSide1);
+
+      Console.WriteLine("Enter a value for side two.");
+      string strSide2 = Console.ReadLine();
+      int side2 = int.Parse(strSide2);
+
+      Console.WriteLine("Enter a value for side three.");
+      string strSide3 = Console.ReadLine();
+      int side3 = int.Parse(strSide3);
+
+      Triangle TriangleEquilateral = new Triangle();
+      Console.WriteLine(TriangleEquilateral.IsTriangle(side1, side2, side3));
+
+    }
+
     public string IsTriangle(int side1, int side2, int side3)
     {
       // string result = "Equilateral";
-      if(side1 == side2 && side2 == side3 && side1 == side3)
+      if (side1 == side2 && side2 == side3 && side1 == side3)
       {
         return "Equilateral";
       }
---
+++ TriangleTracker/Program.cs
@@ -0,0 +1,12 @@
+using System;
+// using System.Collections.Generic;
+using TriangleTracker.Models;
+
+namespace TriangleTracker
+{
+
+  public class Program
+  {
+
+  }
+}
