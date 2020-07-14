41f90b6c45935cbcb5dd9ddd9fae5d35276815cc
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-14 12:30:34 -0600

    straighten out test folders and corresponding code.
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

---
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleTracker;
+
+namespace TriangleTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+    
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    }
+    // Test methods go here
+  }
+}
---
+++ TriangleTracker/Models/Triangle.cs
@@ -0,0 +1,7 @@
+namespace TriangleTracker
+{
+  public class Triangle 
+  {
+    //properties, constructors, methods, etc. go here.
+  }
+}
